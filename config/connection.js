23dd629571478e93fceddab0bdc9d3635a204310
Steven Rodriguez <srrodrig.rodriguez@gmail.com>
2020-10-21 22:30:16 -0500

    added heroku db path

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Password1",
-  database: "burgers_db"
+  user: "u5cp61ta4jl4hqch",
+  password: "bv083980t7dx97s5",
+  database: "c6w85tbis2zvqlxw"
 });
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Password1",
+//   database: "burgers_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
